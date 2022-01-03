fa2d244077fe12ccf1490e94046fd7351dae19a4
Sklyarov Egor <sky.it97@gmail.com>
2022-01-03 13:59:56 +0400

    Swift project

---
+++ Package.swift
@@ -0,0 +1,19 @@
+// swift-tools-version:5.5
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Swiftogram",
+    dependencies: [
+        
+    ],
+    targets: [
+        .executableTarget(
+            name: "Swiftogram",
+            dependencies: []),
+        .testTarget(
+            name: "SwiftogramTests",
+            dependencies: ["Swiftogram"]),
+    ]
+)
