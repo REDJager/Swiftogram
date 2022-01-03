d2c32d1bc60a40b712891e413da648c0cd3982e4
Sklyarov Egor <sky.it97@gmail.com>
2022-01-03 14:17:52 +0400

    Perfect project

--- Package.swift
+++ Package.swift
@@ -5,13 +5,17 @@ import PackageDescription
 
 let package = Package(
     name: "Swiftogram",
+    products: [
+        .executable(name: "Swiftogram", targets: ["Swiftogram"]),
+    ],
     dependencies: [
-        
+        .package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git",
+                 from: "3.0.0"),
     ],
     targets: [
         .executableTarget(
             name: "Swiftogram",
-            dependencies: []),
+            dependencies: [.product(name: "PerfectHTTPServer", package: "Perfect-HTTPServer")]),
         .testTarget(
             name: "SwiftogramTests",
             dependencies: ["Swiftogram"]),
--- Sources/Swiftogram/main.swift
+++ Sources/Swiftogram/main.swift
@@ -1,1 +1,15 @@
-print("Hello, world!")
+import PerfectHTTP
+import PerfectHTTPServer
+
+var routes = Routes()
+
+do {
+    try HTTPServer.launch(
+        name: "localhost",
+        port: 8080,
+        routes: routes,
+        responseFilters: [(HTTPFilter.contentCompression(data: [:]), HTTPFilterPriority.high)]
+    )
+} catch {
+    print(error.localizedDescription)
+}
